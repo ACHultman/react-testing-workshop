b9dd2aedee96ecba1878afb17d17a7c00e4db201
ACHultman <ahultman0@gmail.com>
2021-03-24 15:12:17 -0700

    fix: add axios

--- src/api/quote.ts
+++ src/api/quote.ts
@@ -1,10 +1,16 @@
+import axios from "axios";
+
 export const getKanyeQuote = async () => {
-  const data: any = await fetch("https://api.kanye.rest/");
-  if (!data || !data.body) console.error("Kanye Rest could not be reached.");
-  else {
-    let quote = await data.json();
-    quote = quote.quote;
-    if (!quote || quote.length < 1) return "---";
-    return quote;
+  try {
+    const response = await axios.get("https://api.kanye.rest/");
+    if (!response || response.status !== 200 || !response.data)
+      throw new Error("Kanye Rest could not be reached.");
+    else {
+      let quote = await response.data.quote;
+      if (!quote || quote.length < 1) return "---";
+      return quote;
+    }
+  } catch (error) {
+    console.error(error);
   }
 };
